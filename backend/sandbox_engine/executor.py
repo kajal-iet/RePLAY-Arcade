89ee9cac266ea4378bce47d0e895001c2b0a50db
kajal patel <kajal1202patel@gmail.com>
2026-01-28 23:22:33 +0530

    added Docs files

--- backend/sandbox_engine/executor.py
+++
@@ -1,8 +0,0 @@
-def execute_user_code(code: str, fn_name: str, args: dict):
-    scope = {}
-    exec(code, {}, scope)
-
-    if fn_name not in scope:
-        raise Exception(f"Function '{fn_name}' must be defined")
-
-    return scope[fn_name](**args)
